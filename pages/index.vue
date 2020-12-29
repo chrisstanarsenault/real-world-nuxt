5d12cb30db1456caafcc9815f5ec7ce8b95da2e0
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-12-28 21:02:57 -0500

    finished Using Vuex

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,15 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard,
   },
-  async asyncData({ $axios, error }) {
+  async fetch({ store, error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        events: data,
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
@@ -31,6 +29,9 @@ export default {
       })
     }
   },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
   head() {
     return {
       title: 'Event Listing',
