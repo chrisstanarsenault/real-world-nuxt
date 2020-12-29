ae58bedb3ad61e5726b62ee5cbb559106521399b
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-12-28 21:41:19 -0500

    configured for static deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService.js'
+import Eventservice from './services/EventService.js'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -41,4 +43,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((res) => {
+        return res.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    },
+  },
 }
